e0b9a215ede6a1b48c771add63618940a8f32448
xucz <xuczgh@gmail.com>
2021-12-05 22:56:09 +0800

    feat: 划分菜单功能

--- src/configs/routes.ts
+++ src/configs/routes.ts
@@ -1,76 +1,78 @@
 import React from 'react';
 import { BrowserRouterProps } from 'react-router-dom';
-import Home from 'pages/Home';
-import Monitor from 'pages/Monitor';
-import Report from 'pages/Report';
-import Form from 'pages/Form';
-import StepForm from 'pages/StepForm';
-import UploadCollection from 'pages/UploadCollection';
-import ComplexForm from 'pages/ComplexForm';
-import CustomTable from 'pages/CustomTable';
-import ComplexTable from 'pages/ComplexTable';
-import BaseList from 'pages/List/Base';
-import CardList from 'pages/CardList';
-import Editor from 'pages/Editor';
+import DashboardBase from 'pages/Dashboard/Base';
+import DashboardDetail from 'pages/Dashboard/Detail';
+import ListBase from 'pages/List/Base';
+import ListCard from 'pages/List/Card';
+import ListSelect from 'pages/List/Select';
+import ListTree from 'pages/List/Tree';
+import FormBase from 'pages/Form/Base';
+import FormStep from 'pages/Form/Step';
+import DetailBase from 'pages/Detail/Base';
+import DetailAdvanced from 'pages/Detail/Advanced';
+import DetailDeploy from 'pages/Detail/Deploy';
+import DetailSecondary from 'pages/Detail/Secondary';
 
 interface IRouteItem {
-  key?: string; // 无key时，没有权限校验
+  key?: string;
   path: string;
   exact?: boolean;
   Component: React.FC<BrowserRouterProps>;
 }
 
 export const routes: IRouteItem[] = [
+  // {
+  //   key: 'home',
+  //   path: '/',
+  //   exact: true,
+  //   Component: Home,
+  // },
   {
-    key: 'home',
-    path: '/',
-    exact: true,
-    Component: Home,
+    path: '/dashboard/base',
+    Component: DashboardBase,
   },
   {
-    path: '/monitor',
-    Component: Monitor,
-  },
-  {
-    path: '/report',
-    Component: Report,
-  },
-  {
-    // key: 'form',
-    path: '/form',
-    Component: Form,
-  },
-  {
-    path: '/step-form',
-    Component: StepForm,
-  },
-  {
-    path: '/upload-collection',
-    Component: UploadCollection,
-  },
-  {
-    // key: 'form',
-    path: '/complex-form',
-    Component: ComplexForm,
-  },
-  {
-    path: '/custom-table',
-    Component: CustomTable,
-  },
-  {
-    path: '/complex-table',
-    Component: ComplexTable,
-  },
-  {
-    path: '/editor',
-    Component: Editor,
+    path: '/dashboard/detail',
+    Component: DashboardDetail,
   },
   {
     path: '/list/base',
-    Component: BaseList,
+    Component: ListBase,
   },
   {
-    path: '/card-list',
-    Component: CardList,
+    path: '/list/card',
+    Component: ListCard,
+  },
+  {
+    path: '/list/select',
+    Component: ListSelect,
+  },
+  {
+    path: '/list/tree',
+    Component: ListTree,
+  },
+  {
+    path: '/form/base',
+    Component: FormBase,
+  },
+  {
+    path: '/form/step',
+    Component: FormStep,
+  },
+  {
+    path: '/detail/base',
+    Component: DetailBase,
+  },
+  {
+    path: '/detail/advanced',
+    Component: DetailAdvanced,
+  },
+  {
+    path: '/detail/deploy',
+    Component: DetailDeploy,
+  },
+  {
+    path: '/detail/secondary',
+    Component: DetailSecondary,
   },
 ];
---
+++ src/pages/Result/403/index.tsx
@@ -0,0 +1,5 @@
+import React, { memo } from 'react';
+
+export default memo(() => {
+  return <div>hello</div>;
+});
