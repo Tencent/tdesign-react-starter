de62e97bd12825aca5c76d45313e1750d53d81c1
victorWuxz <614510197@qq.com>
2021-12-17 15:41:18 +0800

    增加card组件

---
+++ src/components/Card/index.tsx
@@ -0,0 +1,59 @@
+import React, { memo, ReactNode } from 'react';
+import cx from 'classnames';
+import { getPrefixCls } from 'utils';
+import './index.less';
+
+export interface Styles {
+  [key: string]: string | number;
+}
+
+export interface CardProps {
+  title?: ReactNode;
+  extra?: ReactNode;
+  actions?: React.ReactNode[];
+  className?: string;
+  prefixCls?: string;
+  borded?: boolean;
+  style?: React.CSSProperties;
+  bodyStyle: React.CSSProperties;
+  headStyle?: React.CSSProperties;
+}
+
+const Card: React.FC<CardProps> = (props) => {
+  const { title, extra, actions, className, prefixCls, borded, style, headStyle, bodyStyle } = props;
+
+  const cls = getPrefixCls('card', prefixCls);
+  const borderCls = `${cls}-borderd`;
+
+  const hasHead = Boolean(title || extra);
+
+  return (
+    <div style={style} className={cx(cls, className, { [borderCls]: borded })}>
+      {hasHead && (
+        <div className={cx(`${cls}-head`)} style={headStyle}>
+          {title && <div className={cx(`${cls}-head-title`)}>{title}</div>}
+          {extra && <div className={cx(`${cls}-head-extra`)}>{extra}</div>}
+        </div>
+      )}
+      <div className={cx(`${cls}-body`)} style={bodyStyle}>
+        {props.children}
+      </div>
+      {actions?.length && (
+        <ul className={cx(`${cls}-actions`)}>
+          {actions.map((action, index) => (
+            <li style={{ width: `${100 / actions.length}%` }} key={`action-${index}`}>
+              <span>{action}</span>
+            </li>
+          ))}
+        </ul>
+      )}
+    </div>
+  );
+};
+
+Card.defaultProps = {
+  style: { width: 300 },
+  borded: true,
+};
+
+export default memo(Card);
