38596fa66fb281963e037f6293e08b7259d35fe0
xucz <xuczgh@gmail.com>
2021-12-18 20:39:58 +0800

    feat: add base list

--- src/modules/store.ts
+++ src/modules/store.ts
@@ -2,23 +2,9 @@ import { configureStore, combineReducers } from '@reduxjs/toolkit';
 import { TypedUseSelectorHook, useSelector, useDispatch } from 'react-redux';
 
 import global from './global';
-import form from './form';
-import stepForm from './stepForm';
-import complexForm from './complexForm';
-import customTable from './customTable';
-import complexTable from './complexTable';
-import chart from './chart';
-import report from './report';
 
 const reducer = combineReducers({
   global,
-  form,
-  stepForm,
-  complexForm,
-  customTable,
-  complexTable,
-  chart,
-  report,
 });
 
 const store = configureStore({
