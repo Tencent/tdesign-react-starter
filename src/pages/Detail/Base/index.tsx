9a8c3d677a03f98d4c866b6e23bd9ad55ed64438
yueveron <yueveron@gmail.com>
2021-12-22 10:31:14 +0800

    feat: 1.安装新版本tdesing-react；CardBox组件;页面layout； --story=870934135 【TDesign】详情页-基础详情页

--- src/pages/Detail/Base/index.tsx
+++ src/pages/Detail/Base/index.tsx
@@ -1,41 +1,76 @@
 import React, { memo } from 'react';
 import classnames from 'classnames';
+import { Steps } from 'tdesign-react';
 import PageBox from 'components/PageBox';
 import CardBox from 'components/CardBox';
 import Style from './index.module.less';
 
-// Mock Data
+const { StepItem } = Steps;
+
+// Mock Data of 基本信息
 interface InfoItem {
   name: string;
   value: string;
   type?: string;
 }
-const data: InfoItem[] = [
+const dataInfo: InfoItem[] = [
   { name: '合同名称', value: '总部办公用品采购项目' },
   { name: '合同状态', value: '履行中', type: 'status' },
   { name: '合同编号', value: 'BH00010' },
   { name: '合同类型', value: '主合同' },
-  { name: '合同类型', value: '主合同' },
-  { name: '合同类型', value: '主合同' },
+  { name: '合同收付类型', value: '付款' },
+  { name: '合同金额', value: '5,000,000元' },
+  { name: '甲方', value: '腾讯科技（深圳）有限公司' },
+  { name: '乙方', value: '欧尚' },
+  { name: '合同签订日期', value: '2020-12-20' },
+  { name: '合同生效日期', value: '2021-01-20' },
+  { name: '合同结束日期', value: '2022-12-20' },
+  { name: '合同附件', value: '总部办公用品采购项目合同.pdf', type: 'link' },
+  { name: '备注', value: '--' },
+  { name: '创建时间', value: '2020-12-22 10:00:00' },
 ];
 
-export default memo(() => {
-  const temp = 'Hello';
-  return (
-    <PageBox withColor={false} withPadding={false}>
-      <CardBox titleName='基本信息' withMarginTop={false}>
-        <div className={classnames(Style.infoBox)}>
-          {data.map((item, index: number) => (
-            <div key={index} className={classnames(Style.infoBoxItem)}>
-              <h1>{item.name}</h1>
-              <span>{item.value}</span>
-            </div>
+// Mock Data of 变更记录
+interface StepItem {
+  name: string;
+  detail?: string;
+}
+const dataStep: StepItem[] = [
+  { name: '上传合同附件', detail: '这里是提示文字' },
+  { name: '修改合同金额', detail: '这里是提示文字' },
+  { name: '新建合同', detail: '2020-12-01 15:00:00 管理员-李川操作' },
+];
+const setpCurrent = 1;
+
+export default memo(() => (
+  <PageBox withColor={false} withPadding={false}>
+    <CardBox titleName='基本信息' withMarginTop={false}>
+      <div className={classnames(Style.infoBox)}>
+        {dataInfo.map((item, index: number) => (
+          <div key={index} className={classnames(Style.infoBoxItem)}>
+            <h1>{item.name}</h1>
+            <span
+              className={classnames({
+                [Style.inProgress]: item.type === 'status',
+                [Style.pdf]: item.type === 'link',
+              })}
+            >
+              {item.type === 'status' && <i></i>}
+              {item.value}
+            </span>
+          </div>
+        ))}
+      </div>
+    </CardBox>
+    <CardBox titleName='变更记录'>
+      <div>
+        <Steps layout='vertical' theme='dot' current={setpCurrent}>
+          {dataStep.map((item, index: number) => (
+            <StepItem key={index} title={item.name} content={item.detail} />
           ))}
-        </div>
-      </CardBox>
-      <CardBox titleName='变更记录'>
-        <div>{temp}</div>
-      </CardBox>
-    </PageBox>
-  );
-});
+        </Steps>
+      </div>
+    </CardBox>
+  </PageBox>
+));
+<i></i>;
