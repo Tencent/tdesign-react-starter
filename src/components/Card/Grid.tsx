2bb89f57d0c668e2e423b3053c4a46feb9c47a15
victorWuxz <614510197@qq.com>
2021-12-27 12:02:27 +0800

    card支持响应式

--- src/components/Card/Grid.tsx
+++ src/components/Card/Grid.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import cx from 'classnames';
-import { getPrefixCls } from 'utils/utils';
+import { getPrefixCls, getWidth } from 'utils/utils';
 
 export interface GridProps {
   prefixCls?: string;
@@ -8,19 +8,29 @@ export interface GridProps {
   className?: string;
   style?: React.CSSProperties;
   children?: React.ReactNode;
+  xs?: number;
+  sm?: number;
+  md?: number;
+  lg?: number;
+  xl?: number;
+  xxl?: number;
 }
 
 const Grid: React.FC<GridProps> = (props) => {
-  const { hoverable, className, style, prefixCls } = props;
+  const { hoverable, className, style, prefixCls, xs, sm, md, lg, xl, xxl } = props;
 
   const cls = `${getPrefixCls('card', prefixCls)}-grid`;
   const hoverableCls = `${cls}-hoverable`;
+
+  const width = getWidth({ xs, sm, md, lg, xl, xxl });
+  const isEffectWidth = width !== undefined;
 
   return (
     <div
       style={style}
       className={cx(cls, className, {
         [hoverableCls]: hoverable,
+        [`${cls}-${width}`]: isEffectWidth,
       })}
     >
       {props.children}
