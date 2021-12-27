57e1e77adaf186734549444f0bfa27014e101c63
victorWuxz <614510197@qq.com>
2021-12-27 17:36:32 +0800

    card支持响应式

--- src/components/Card/Grid.tsx
+++ src/components/Card/Grid.tsx
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import cx from 'classnames';
+import throttle from 'lodash/throttle';
 import { getPrefixCls, getWidth } from 'utils/utils';
 
 export interface GridProps {
@@ -8,6 +9,7 @@ export interface GridProps {
   className?: string;
   style?: React.CSSProperties;
   children?: React.ReactNode;
+  span?: number;
   xs?: number;
   sm?: number;
   md?: number;
@@ -17,20 +19,32 @@ export interface GridProps {
 }
 
 const Grid: React.FC<GridProps> = (props) => {
-  const { hoverable, className, style, prefixCls, xs, sm, md, lg, xl, xxl } = props;
+  const { hoverable, className, style, prefixCls, span, xs, sm, md, lg, xl, xxl } = props;
 
   const cls = `${getPrefixCls('card', prefixCls)}-grid`;
   const hoverableCls = `${cls}-hoverable`;
+  const width = getWidth({ xs, sm, md, lg, xl, xxl, span });
+  const [colValue, setColValue] = useState(width);
 
-  const width = getWidth({ xs, sm, md, lg, xl, xxl });
-  const isEffectWidth = width !== undefined;
+  const isEffectWidth = colValue !== undefined;
+
+  useEffect(() => {
+    const getColValue = throttle(() => {
+      const width = getWidth({ xs, sm, md, lg, xl, xxl, span });
+      setColValue(width);
+    }, 500);
+    window.addEventListener('resize', getColValue);
+    return () => {
+      window.removeEventListener('resize', getColValue);
+    };
+  }, []);
 
   return (
     <div
       style={style}
       className={cx(cls, className, {
         [hoverableCls]: hoverable,
-        [`${cls}-${width}`]: isEffectWidth,
+        [`${cls}-${colValue}`]: isEffectWidth,
       })}
     >
       {props.children}
