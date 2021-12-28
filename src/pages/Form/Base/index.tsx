c432951b4800a2ce09c15fe60ac6a12b783ac947
Xiaotou-Sophia <1376203691@qq.com>
2021-12-28 19:32:53 +0800

    feature:  基础表单  --story=858352215

--- src/pages/Form/Base/index.tsx
+++ src/pages/Form/Base/index.tsx
@@ -1,5 +1,18 @@
 import React, { memo, useState } from 'react';
-import { Form, Input, Radio, Button, DatePicker, Select, Textarea, Avatar, AvatarGroup } from 'tdesign-react';
+import {
+  Form,
+  Row,
+  Col,
+  Input,
+  Radio,
+  Button,
+  DatePicker,
+  Select,
+  Textarea,
+  Avatar,
+  AvatarGroup,
+  Upload,
+} from 'tdesign-react';
 import PageBox from 'components/PageBox';
 import Style from './index.module.less';
 
@@ -15,84 +28,109 @@ export default memo(() => {
   const onRemarkChange = (value: any) => {
     console.log(value);
   };
+
+  const handleFail = ({ file }: { file: any }) => {
+    console.error(`文件 ${file.name} 上传失败`);
+  };
+
   return (
-    <PageBox>
-      <div className={Style.formWrapper}>
-        <div className={Style.titleBox}>
-          <div className={Style.titleText}>合同信息</div>
-        </div>
-        <Form onSubmit={onSubmit} onReset={onReset} colon labelWidth={100} labelAlign={labelAlign}>
-          <FormItem label='合同名称' name='name'>
-            <Input placeholder='请输入内容' />
-          </FormItem>
-          <FormItem label='合同类型' name='type'>
-            <Select value='A' onChange={onSelectChange} style={{ width: '40%' }} clearable placeholder='请选择类型'>
-              <Option key='A' label='类型A' value='A' />
-              <Option key='B' label='类型B' value='B' />
-              <Option key='C' label='类型C' value='C' />
-            </Select>
+    <PageBox withPadding={false}>
+      <div className={Style.formContainer}>
+        <Form
+          onSubmit={onSubmit}
+          onReset={onReset}
+          colon
+          labelWidth={100}
+          labelAlign={labelAlign}
+          className={Style.baseForm}
+        >
+          <div className={Style.titleBox}>
+            <div className={Style.titleText}>合同信息</div>
+          </div>
+          <Row gutter={[32, 24]}>
+            <Col span={6}>
+              <FormItem label='合同名称' name='name'>
+                <Input placeholder='请输入内容' />
+              </FormItem>
+            </Col>
+            <Col span={6}>
+              <FormItem label='合同类型' name='type'>
+                <Select value='A' onChange={onSelectChange} placeholder='请选择类型'>
+                  <Option key='A' label='类型A' value='A' />
+                  <Option key='B' label='类型B' value='B' />
+                  <Option key='C' label='类型C' value='C' />
+                </Select>
+              </FormItem>
+            </Col>
+
+            <Col span={12}>
+              <FormItem label='合同收付类型' name='payment'>
+                <Radio.Group>
+                  <Radio value='0'>收款</Radio>
+                  <Radio value='1'>付款</Radio>
+                </Radio.Group>
+                <Input placeholder='请输入金额' style={{ width: '25%' }} />
+              </FormItem>
+            </Col>
+
+            <Col span={6}>
+              <FormItem label='甲方' name='partyA'>
+                <Select value='A' onChange={onSelectChange} placeholder='请选择类型'>
+                  <Option key='A' label='公司A' value='A' />
+                  <Option key='B' label='公司B' value='B' />
+                  <Option key='C' label='公司C' value='C' />
+                </Select>
+              </FormItem>
+            </Col>
+            <Col span={6}>
+              <FormItem label='乙方' name='partyB'>
+                <Select value='A' onChange={onSelectChange} placeholder='请选择类型'>
+                  <Option key='A' label='公司A' value='A' />
+                  <Option key='B' label='公司B' value='B' />
+                  <Option key='C' label='公司C' value='C' />
+                </Select>
+              </FormItem>
+            </Col>
+
+            <Col span={6} className={Style.dateCol}>
+              <FormItem label='合同签订日期' name='signDate'>
+                <DatePicker mode='date' />
+              </FormItem>
+            </Col>
+            <Col span={6} className={Style.dateCol}>
+              <FormItem label='合同生效日期' name='effectiveDate'>
+                <DatePicker mode='date' />
+              </FormItem>
+            </Col>
+            <Col span={6} className={Style.dateCol}>
+              <FormItem label='合同结束日期' name='endDate'>
+                <DatePicker mode='date' />
+              </FormItem>
+            </Col>
+          </Row>
+          <div className={Style.titleBox}>
+            <div className={Style.titleText}>其他信息</div>
+          </div>
+          <FormItem label='备注' name='remark'>
+            <Textarea placeholder='请输入备注' value={'哈哈哈'} onChange={onRemarkChange} />
           </FormItem>
 
-          <FormItem label='合同收付类型' name='payment'>
-            <Radio.Group>
-              <Radio value='0'>收款</Radio>
-              <Radio value='1'>付款</Radio>
-            </Radio.Group>
-            <Input placeholder='请输入金额' style={{ width: '40%' }} />
-          </FormItem>
-
-          <FormItem label='甲方' name='partyA'>
-            <Select value='A' onChange={onSelectChange} style={{ width: '40%' }} clearable placeholder='请选择类型'>
-              <Option key='A' label='公司A' value='A' />
-              <Option key='B' label='公司B' value='B' />
-              <Option key='C' label='公司C' value='C' />
-            </Select>
-          </FormItem>
-
-          <FormItem label='乙方' name='partyB'>
-            <Select value='A' onChange={onSelectChange} style={{ width: '40%' }} clearable placeholder='请选择类型'>
-              <Option key='A' label='公司A' value='A' />
-              <Option key='B' label='公司B' value='B' />
-              <Option key='C' label='公司C' value='C' />
-            </Select>
-          </FormItem>
-          <FormItem label='合同签订日期' name='signDate'>
-            <DatePicker mode='date' />
-          </FormItem>
-
-          <FormItem label='合同生效日期' name='effectiveDate'>
-            <DatePicker mode='date' />
-          </FormItem>
-
-          <FormItem label='合同结束日期' name='endDate'>
-            <DatePicker mode='date' />
+          <FormItem label='公证人' name='notary'>
+            <AvatarGroup>
+              <Avatar>D</Avatar>
+              <Avatar>S</Avatar>
+              <Avatar>+</Avatar>
+            </AvatarGroup>
           </FormItem>
         </Form>
       </div>
-      <div className={Style.formWrapper}>
-        <div className={Style.titleBox}>
-          <div className={Style.titleText}>其他信息</div>
-          <Form>
-            <FormItem label='备注' name='remark'>
-              <Textarea placeholder='请输入备注' value={'哈哈哈'} onChange={onRemarkChange} />
-            </FormItem>
-
-            <FormItem label='公证人' name='notary'>
-              <AvatarGroup>
-                <Avatar>D</Avatar>
-                <Avatar>S</Avatar>
-                <Avatar>+</Avatar>
-              </AvatarGroup>
-            </FormItem>
-            <FormItem>
-              <Button type='submit' theme='primary'>
-                提交
-              </Button>
-              <Button type='reset' style={{ marginLeft: 12 }}>
-                重置
-              </Button>
-            </FormItem>
-          </Form>
+      <div className={Style.bottomContainer}>
+        <div className={Style.uploadBox}>
+          <Upload onFail={handleFail} tips='请上传pdf文件，大小在60M以内' action='' />
+        </div>
+        <div className={Style.buttonsBox}>
+          <Button type='reset'>重置</Button>
+          <Button type='submit'>提交</Button>
         </div>
       </div>
     </PageBox>
