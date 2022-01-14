b74950a62d11298041e9fbeeee542e81faf54acc
ancai <ancaiu9@gmail.com>
2022-01-14 20:51:57 +0800

    feat: replace tvision with echarts --story=123

--- src/pages/Dashboard/Base/index.tsx
+++ src/pages/Dashboard/Base/index.tsx
@@ -1,7 +1,8 @@
 import React, { memo, useState } from 'react';
 import { Row, Col, Radio, Table, Button } from 'tdesign-react';
 import { Icon } from 'tdesign-icons-react';
-import { Tvision2Area, Tvision2Line, Tvision2Bar, Tvision2Pie } from '@tencent/react-tvision2';
+import { Tvision2Bar, Tvision2Pie } from '@tencent/react-tvision2';
+import { Tvision2Line, Tvision2Area } from 'components/Charts';
 import type { TdPrimaryTableProps } from 'tdesign-react/es/table';
 import classnames from 'classnames';
 
