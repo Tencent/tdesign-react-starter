9b3e40c4371b4d73cd49d96a9fdb4307ef236408
xucz <xuczgh@gmail.com>
2022-03-02 11:42:54 +0800

    feat: 简化request

--- src/utils/request.ts
+++ src/utils/request.ts
@@ -4,57 +4,28 @@ import proxy from '../configs/host';
 const env = import.meta?.env?.MODE || 'development';
 const API_HOST = proxy[env].API;
 
-const CODE = {
-  LOGIN_TIMEOUT: 1000,
-  REQUEST_SUCCESS: 0,
-  REQUEST_FORBID: 1001,
-};
+const SUCCESS_CODE = 0;
+const TIMEOUT = 5000;
 
 export const instance = axios.create({
   baseURL: API_HOST,
-  timeout: 5000,
+  timeout: TIMEOUT,
   withCredentials: true,
 });
-
-// eslint-disable-next-line
-// @ts-ignore
-// axios的retry ts类型有问题
-instance.interceptors.retry = 3;
-
-instance.interceptors.request.use((config) => config);
 
 instance.interceptors.response.use(
   // eslint-disable-next-line consistent-return
   (response) => {
     if (response.status === 200) {
       const { data } = response;
-      if (data.code === CODE.REQUEST_SUCCESS) {
+      if (data.code === SUCCESS_CODE) {
         return data;
       }
       return Promise.reject(data);
     }
+    return Promise.reject(response?.data);
   },
-  (err) => {
-    const { config } = err;
-
-    if (!config || !config.retry) return Promise.reject(err);
-
-    config.retryCount = config.retryCount || 0;
-
-    if (config.retryCount >= config.retry) {
-      return Promise.reject(err);
-    }
-
-    config.retryCount += 1;
-
-    const backoff = new Promise((resolve) => {
-      setTimeout(() => {
-        resolve({});
-      }, config.retryDelay || 1);
-    });
-
-    return backoff.then(() => instance(config));
-  },
+  (e) => Promise.reject(e),
 );
 
 export default instance;
