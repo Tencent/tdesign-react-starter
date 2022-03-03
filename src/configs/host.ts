ca57c97092cb514ba7d978daccbdf57f4b9922fc
xucz <xuczgh@gmail.com>
2022-03-03 13:56:42 +0800

    chore: 优化构建

--- src/configs/host.ts
+++ src/configs/host.ts
@@ -11,7 +11,7 @@ export default {
     // 测试环境接口地址
     API: 'https://service-exndqyuk-1257786608.gz.apigw.tencentcs.com',
   },
-  release: {
+  production: {
     // 正式环境接口地址
     API: 'https://service-bv448zsw-1257786608.gz.apigw.tencentcs.com',
   },
