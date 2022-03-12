0dd86166a78a9fff5505772d5a28a5c830577937
xucz <xuczgh@gmail.com>
2022-03-12 19:42:16 +0800

    fix: fix 表单重置失败, fixes #34

--- src/pages/Form/Base/index.tsx
+++ src/pages/Form/Base/index.tsx
@@ -1,4 +1,4 @@
-import React, { memo, useState } from 'react';
+import React, { memo, useRef } from 'react';
 import {
   Form,
   Row,
@@ -13,25 +13,36 @@ import {
   Upload,
   MessagePlugin,
 } from 'tdesign-react';
+import { SubmitContext, FormInstanceFunctions } from 'tdesign-react/es/form/type';
 import PageBox from 'components/PageBox';
 import Style from './index.module.less';
 
 const { FormItem } = Form;
 const { Option } = Select;
-const { Group: AvatarGroup } = Avatar;
+const { Group } = Avatar;
+
+const INITIAL_DATA = {
+  name: '',
+  type: '',
+  payment: '',
+  partyA: '',
+  partyB: '',
+  signDate: '',
+  effectiveDate: '',
+  endDate: '',
+  remark: '',
+  notary: '',
+  file: [],
+};
 
 export default memo(() => {
-  const [labelAlign] = useState('top' as any);
+  const formRef = useRef<FormInstanceFunctions>();
 
-  const onSubmit = (e: any) => {
+  const onSubmit = (e: SubmitContext) => {
     if (e.validateResult === true) {
+      console.log('form 值', formRef.current?.getFieldsValue?.(true));
       MessagePlugin.info('提交成功');
     }
-  };
-  const onReset = () => {};
-  const onSelectChange = () => {};
-  const onRemarkChange = (value: any) => {
-    console.log(value);
   };
 
   const handleFail = ({ file }: { file: any }) => {
@@ -41,7 +52,7 @@ export default memo(() => {
   return (
     <PageBox withPadding={false}>
       <div className={Style.formContainer}>
-        <Form onSubmit={onSubmit} onReset={onReset} labelWidth={100} labelAlign={labelAlign} className={Style.baseForm}>
+        <Form ref={formRef} onSubmit={onSubmit} labelWidth={100} labelAlign='top' className={Style.baseForm}>
           <div className={Style.titleBox}>
             <div className={Style.titleText}>合同信息</div>
           </div>
@@ -50,18 +61,21 @@ export default memo(() => {
               <FormItem
                 label='合同名称'
                 name='name'
+                initialData={INITIAL_DATA.name}
                 rules={[{ required: true, message: '合同名称必填', type: 'error' }]}
               >
                 <Input placeholder='请输入内容' />
               </FormItem>
             </Col>
+
             <Col span={6}>
               <FormItem
                 label='合同类型'
                 name='type'
+                initialData={INITIAL_DATA.type}
                 rules={[{ required: true, message: '合同类型必填', type: 'error' }]}
               >
-                <Select value='A' onChange={onSelectChange} placeholder='请选择类型'>
+                <Select placeholder='请选择类型'>
                   <Option key='A' label='类型A' value='A' />
                   <Option key='B' label='类型B' value='B' />
                   <Option key='C' label='类型C' value='C' />
@@ -70,27 +84,23 @@ export default memo(() => {
             </Col>
 
             <Col span={12}>
-              <FormItem label='合同收付类型' name='payment' rules={[{ required: true }]}>
+              <FormItem
+                label='合同收付类型'
+                name='payment'
+                initialData={INITIAL_DATA.payment}
+                rules={[{ required: true }]}
+              >
                 <Radio.Group>
                   <Radio value='0'>收款</Radio>
                   <Radio value='1'>付款</Radio>
                 </Radio.Group>
-                <Input placeholder='请输入金额' style={{ width: '25%' }} />
+                <Input placeholder='请输入金额' style={{ width: 160 }} />
               </FormItem>
             </Col>
 
             <Col span={6}>
-              <FormItem label='甲方' name='partyA' rules={[{ required: true }]}>
-                <Select value='A' onChange={onSelectChange} placeholder='请选择类型'>
-                  <Option key='A' label='公司A' value='A' />
-                  <Option key='B' label='公司B' value='B' />
-                  <Option key='C' label='公司C' value='C' />
-                </Select>
-              </FormItem>
-            </Col>
-            <Col span={6}>
-              <FormItem label='乙方' name='partyB' rules={[{ required: true }]}>
-                <Select value='A' onChange={onSelectChange} placeholder='请选择类型'>
+              <FormItem label='甲方' name='partyA' initialData={INITIAL_DATA.partyA} rules={[{ required: true }]}>
+                <Select placeholder='请选择类型'>
                   <Option key='A' label='公司A' value='A' />
                   <Option key='B' label='公司B' value='B' />
                   <Option key='C' label='公司C' value='C' />
@@ -98,41 +108,61 @@ export default memo(() => {
               </FormItem>
             </Col>
 
-            <Col span={6} className={Style.dateCol} rules={[{ required: true }]}>
-              <FormItem label='合同签订日期' name='signDate'>
-                <DatePicker mode='date' />
-              </FormItem>
-            </Col>
-            <Col span={6} className={Style.dateCol} rules={[{ required: true }]}>
-              <FormItem label='合同生效日期' name='effectiveDate'>
-                <DatePicker mode='date' />
-              </FormItem>
-            </Col>
-            <Col span={6} className={Style.dateCol} rules={[{ required: true }]}>
-              <FormItem label='合同结束日期' name='endDate'>
-                <DatePicker mode='date' />
-              </FormItem>
-            </Col>
             <Col span={6}>
-              <FormItem label='合同文件' name='endDate'>
-                <Upload onFail={handleFail} tips='请上传pdf文件，大小在60M以内' action='' />
+              <FormItem label='乙方' name='partyB' initialData={INITIAL_DATA.partyB} rules={[{ required: true }]}>
+                <Select value='A' placeholder='请选择类型'>
+                  <Option key='A' label='公司A' value='A' />
+                  <Option key='B' label='公司B' value='B' />
+                  <Option key='C' label='公司C' value='C' />
+                </Select>
+              </FormItem>
+            </Col>
+
+            <Col span={6} className={Style.dateCol} rules={[{ required: true }]}>
+              <FormItem label='合同签订日期' name='signDate' initialData={INITIAL_DATA.signDate}>
+                <DatePicker mode='date' />
+              </FormItem>
+            </Col>
+
+            <Col span={6} className={Style.dateCol} rules={[{ required: true }]}>
+              <FormItem label='合同生效日期' name='effectiveDate' initialData={INITIAL_DATA.effectiveDate}>
+                <DatePicker mode='date' />
+              </FormItem>
+            </Col>
+
+            <Col span={6} className={Style.dateCol} rules={[{ required: true }]}>
+              <FormItem label='合同结束日期' name='endDate' initialData={INITIAL_DATA.endDate}>
+                <DatePicker mode='date' />
+              </FormItem>
+            </Col>
+
+            <Col span={6}>
+              <FormItem label='合同文件' name='file' initialData={INITIAL_DATA.file}>
+                <Upload
+                  onFail={handleFail}
+                  tips='请上传pdf文件，大小在60M以内'
+                  action='//service-bv448zsw-1257786608.gz.apigw.tencentcs.com/api/upload-demo'
+                />
               </FormItem>
             </Col>
           </Row>
+
           <div className={Style.titleBox}>
             <div className={Style.titleText}>其他信息</div>
           </div>
-          <FormItem label='备注' name='remark'>
-            <Textarea placeholder='请输入备注' value={'哈哈哈'} onChange={onRemarkChange} />
+
+          <FormItem label='备注' name='remark' initialData={INITIAL_DATA.remark}>
+            <Textarea placeholder='请输入备注' />
           </FormItem>
 
-          <FormItem label='公证人' name='notary'>
-            <AvatarGroup>
+          <FormItem label='公证人' name='notary' initialData={INITIAL_DATA.notary}>
+            <Group>
               <Avatar>D</Avatar>
               <Avatar>S</Avatar>
               <Avatar>+</Avatar>
-            </AvatarGroup>
+            </Group>
           </FormItem>
+
           <FormItem>
             <Button type='submit' theme='primary'>
               提交
