e7ca90c66ed43f531fb8cddc96a1052269b67a40
Uyarn <uyarnchen@gmail.com>
2022-04-06 10:56:05 +0800

    fix: loss css token

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,8 @@ import { HashRouter } from 'react-router-dom';
 import store from 'modules/store';
 import App from 'layouts/index';
 
+import 'tdesign-react/es/style/index.css';
+
 import './styles/index.less';
 
 const renderApp = () => {
