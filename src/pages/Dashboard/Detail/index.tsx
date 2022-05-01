80aa4626d8a438ca63c259803b40c29c1c7e9e42
xucz <xuchaozheng@outlook.com>
2022-05-01 17:55:35 +0800

    feat: 代码格式优化

--- src/pages/Dashboard/Detail/index.tsx
+++ src/pages/Dashboard/Detail/index.tsx
@@ -3,10 +3,9 @@ import PageBox from 'components/PageBox';
 import MonthPurchase from './components/MonthPurchase';
 import PurchaseTrend from './components/PurchaseTrend';
 import PurchaseSatisfaction from './components/Satisfaction';
-import Style from './index.module.less';
 
 export default memo(() => (
-  <PageBox className={Style.panel}>
+  <PageBox>
     <MonthPurchase />
     <PurchaseTrend />
     <PurchaseSatisfaction />
--- src/pages/Detail/Deploy/components/TopChart.tsx
+++ src/pages/Detail/Deploy/components/TopChart.tsx
@@ -19,7 +19,7 @@ const TopChart = () => {
   return (
     <Row gutter={16} className={Style.panel}>
       <Col span={6}>
-        <Card title='部署趋势'>
+        <Card title='部署趋势' header>
           <div className={Style.deployPanelLeft}>
             <DynamicLineChart />
           </div>
