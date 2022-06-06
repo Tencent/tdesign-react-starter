611c212de092111b621c7324ec6bf2833d890fd8
yuyang <uyarnchen@gmail.com>
2022-06-06 20:14:39 +0800

    fix(datepicker): refactor date-picker (#87)

--- src/components/DatePicker/index.tsx
+++ src/components/DatePicker/index.tsx
@@ -1,18 +1,19 @@
 import React from 'react';
-import { DatePicker } from 'tdesign-react';
-import type { DateValue } from 'tdesign-react/es/date-picker';
-import dayjs, { Dayjs } from 'dayjs';
+import { DateRangePicker, DateRangeValue } from 'tdesign-react';
+import dayjs from 'dayjs';
 
-const RECENT_7_DAYS: [Dayjs, Dayjs] = [dayjs().subtract(7, 'day'), dayjs().subtract(1, 'day')];
+const RECENT_7_DAYS: DateRangeValue = [
+  dayjs().subtract(7, 'day').format('YYYY-MM-DD'),
+  dayjs().subtract(1, 'day').format('YYYY-MM-DD'),
+];
 
-const LastWeekDatePicker = (onChange: (value: Array<string>) => void) => (
-  <DatePicker
-    style={{ width: 240 }}
+const LastWeekDatePicker = (onChange: (value: DateRangeValue) => void) => (
+  <DateRangePicker
     mode='date'
-    range
-    placeholder="['开始时间', '结束时间']"
-    defaultValue={RECENT_7_DAYS.map((item) => item.toDate())}
-    onChange={(value: DateValue) => onChange(value as string[])}
+    placeholder={['开始时间', '结束时间']}
+    value={RECENT_7_DAYS}
+    format='YYYY-MM-DD'
+    onChange={(value) => onChange(value)}
   />
 );
 
