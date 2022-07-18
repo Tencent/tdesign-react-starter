6d8302c29634f68e715ad70798e060d04be6c320
xucz <xuchaozheng@outlook.com>
2022-07-18 23:35:53 +0800

    fix: fixed theme color

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import store from 'modules/store';
 import App from 'layouts/index';
 
 import 'tdesign-react/es/style/index.css';
-
+import './styles/theme.less';
 import './styles/index.less';
 
 const renderApp = () => {
