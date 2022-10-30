627c90a88b180137c4e2de1a79ee17942881a110
xucz <xuchaozheng@outlook.com>
2022-10-30 23:05:19 +0800

    feat: 使用space组件

--- src/layouts/components/Header/HeaderIcon.tsx
+++ src/layouts/components/Header/HeaderIcon.tsx
@@ -1,6 +1,6 @@
 import React, { memo } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { Button, Popup, Badge, Dropdown, Row, Col } from 'tdesign-react';
+import { Button, Popup, Badge, Dropdown, Space } from 'tdesign-react';
 import {
   Icon,
   LogoGithubIcon,
@@ -40,79 +40,61 @@ export default memo(() => {
   };
 
   return (
-    <Row align='middle' justify='center'>
-      <Col>
-        <Badge
-          className={Style.badge}
-          count={6}
-          dot={false}
-          maxCount={99}
-          shape='circle'
-          showZero={false}
-          size='medium'
-        >
-          <Button className={Style.menuIcon} shape='square' size='large' variant='text' icon={<MailIcon />} />
-        </Badge>
-      </Col>
-      <Col>
-        <Popup content='代码仓库' placement='bottom' showArrow destroyOnClose>
-          <Button
-            className={Style.menuIcon}
-            shape='square'
-            size='large'
-            variant='text'
-            onClick={gotoGitHub}
-            icon={<LogoGithubIcon />}
-          />
-        </Popup>
-      </Col>
-      <Col>
-        <Popup content='帮助文档' placement='bottom' showArrow destroyOnClose>
-          <Button
-            className={Style.menuIcon}
-            shape='square'
-            size='large'
-            variant='text'
-            onClick={gotoWiki}
-            icon={<HelpCircleIcon />}
-          />
-        </Popup>
-      </Col>
-      <Col>
-        <Dropdown trigger={'click'} onClick={clickHandler}>
-          <Button variant='text' className={Style.dropdown}>
-            <Icon name='user-circle' className={Style.icon} />
-            <span className={Style.text}>Tencent</span>
-            <Icon name='chevron-down' className={Style.icon} />
-          </Button>
-          <DropdownMenu>
-            <DropdownItem value={1}>
-              <div className={Style.dropItem}>
-                <UserCircleIcon />
-                <span>个人中心</span>
-              </div>
-            </DropdownItem>
-            <DropdownItem value={1} onClick={handleLogout}>
-              <div className={Style.dropItem}>
-                <PoweroffIcon />
-                <span>退出登录</span>
-              </div>
-            </DropdownItem>
-          </DropdownMenu>
-        </Dropdown>
-      </Col>
-      <Col>
-        <Popup content='页面设置' placement='bottom' showArrow destroyOnClose>
-          <Button
-            className={Style.menuIcon}
-            shape='square'
-            size='large'
-            variant='text'
-            onClick={() => dispatch(toggleSetting())}
-            icon={<SettingIcon />}
-          />
-        </Popup>
-      </Col>
-    </Row>
+    <Space align='center'>
+      <Badge className={Style.badge} count={6} dot={false} maxCount={99} shape='circle' showZero={false} size='medium'>
+        <Button className={Style.menuIcon} shape='square' size='large' variant='text' icon={<MailIcon />} />
+      </Badge>
+      <Popup content='代码仓库' placement='bottom' showArrow destroyOnClose>
+        <Button
+          className={Style.menuIcon}
+          shape='square'
+          size='large'
+          variant='text'
+          onClick={gotoGitHub}
+          icon={<LogoGithubIcon />}
+        />
+      </Popup>
+      <Popup content='帮助文档' placement='bottom' showArrow destroyOnClose>
+        <Button
+          className={Style.menuIcon}
+          shape='square'
+          size='large'
+          variant='text'
+          onClick={gotoWiki}
+          icon={<HelpCircleIcon />}
+        />
+      </Popup>
+      <Dropdown trigger={'click'} onClick={clickHandler}>
+        <Button variant='text' className={Style.dropdown}>
+          <Icon name='user-circle' className={Style.icon} />
+          <span className={Style.text}>Tencent</span>
+          <Icon name='chevron-down' className={Style.icon} />
+        </Button>
+        <DropdownMenu>
+          <DropdownItem value={1}>
+            <div className={Style.dropItem}>
+              <UserCircleIcon />
+              <span>个人中心</span>
+            </div>
+          </DropdownItem>
+          <DropdownItem value={1} onClick={handleLogout}>
+            <div className={Style.dropItem}>
+              <PoweroffIcon />
+              <span>退出登录</span>
+            </div>
+          </DropdownItem>
+        </DropdownMenu>
+      </Dropdown>
+      <Popup content='页面设置' placement='bottom' showArrow destroyOnClose>
+        <Button
+          className={Style.menuIcon}
+          shape='square'
+          size='large'
+          variant='text'
+          onClick={() => dispatch(toggleSetting())}
+          icon={<SettingIcon />}
+        />
+      </Popup>
+    </Space>
   );
 });
