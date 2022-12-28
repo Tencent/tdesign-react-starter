ea181bf4e2be388c525618021aac523164745b5b
yuyang <uyarnchen@gmail.com>
2022-12-28 15:50:55 +0800

    fix: site path and use history router (#136)
    * feat: 代码优化 (#22)
    
    * feat: 代码优化
    
    * fix: fix card title
    
    * fix: fix color
    
    * fix: 修复饼图文字重叠 (#57)
    
    * chore(deps): bump axios from 0.21.4 to 0.26.1 (#54)
    
    Bumps [axios](https://github.com/axios/axios) from 0.21.4 to 0.26.1.
    - [Release notes](https://github.com/axios/axios/releases)
    - [Changelog](https://github.com/axios/axios/blob/master/CHANGELOG.md)
    - [Commits](https://github.com/axios/axios/compare/v0.21.4...v0.26.1)
    
    ---
    updated-dependencies:
    - dependency-name: axios
      dependency-type: direct:production
      update-type: version-update:semver-minor
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    
    * fix: 修复图表文字重叠, fixes #50
    
    * fix: loss css token (#58)
    
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    Co-authored-by: yuyang <uyarnchen@gmail.com>
    
    * feat: add maintenance page (#73)
    
    * fix(typo): fix form menu title typo (#74)
    
    * chore(deps): bump qrcode.react from 3.0.1 to 3.0.2
    
    Bumps [qrcode.react](https://github.com/zpao/qrcode.react) from 3.0.1 to 3.0.2.
    - [Release notes](https://github.com/zpao/qrcode.react/releases)
    - [Changelog](https://github.com/zpao/qrcode.react/blob/main/CHANGELOG.md)
    - [Commits](https://github.com/zpao/qrcode.react/compare/v3.0.1...v3.0.2)
    
    ---
    updated-dependencies:
    - dependency-name: qrcode.react
      dependency-type: direct:production
      update-type: version-update:semver-patch
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    * fix(datepicker): refactor date-picker (#87)
    
    * fix: site path and use history router
    
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: 巅峰召唤师cz <xuczgh@gmail.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    Co-authored-by: 巅峰召唤师cz <xuchaozheng@outlook.com>
    Co-authored-by: southorange1228 <32590310+southorange1228@users.noreply.github.com>

--- src/main.tsx
+++ src/main.tsx
@@ -1,20 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { HashRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import store from 'modules/store';
 import App from 'layouts/index';
 
 import 'tdesign-react/es/style/index.css';
+
 import './styles/theme.less';
 import './styles/index.less';
+
+const env = import.meta.env.MODE || 'development';
+const baseRouterName = env === 'site' ? '/starter/react/' : '';
 
 const renderApp = () => {
   ReactDOM.render(
     <Provider store={store}>
-      <HashRouter>
+      <BrowserRouter basename={baseRouterName}>
         <App />
-      </HashRouter>
+      </BrowserRouter>
     </Provider>,
     document.getElementById('app'),
   );
